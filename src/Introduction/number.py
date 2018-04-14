9067f018d6fe5d70c48b6a025d8e875bcd63c892
hadesyi <leesk55@gmail.com>
2018-04-15 00:42:23 +0900

    Numbers

--- src/Introduction/number.py
+++ src/Introduction/number.py
@@ -8,4 +8,19 @@ print(50 - 5 * 6)
 print((50 - 5 * 6) / 4)
 print(8 / 5)
 
+print(17 / 3)
+print(17 // 3)
+print(17 % 3)
+print(5 * 3 + 2)
+print(5 ** 2)
+print(2 ** 7)
 
+width = 20
+height = 5 * 9
+print(width + height)
+
+print(4 * 3.75 - 1)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
